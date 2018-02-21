8470c666acdb86c3149976af47b381615e179b54
fouralarmfire <claudiaberesford@googlemail.com>
2018-02-21 20:38:55 +0000

    part 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def is_divisible_by_three?(number)
   number % 3 == 0
 end
+
+def is_divisible_by_five?(number)
+  number % 5 == 0
+end
+
+def is_divisible_by_three_and_five?(number)
+  number % 15 == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,5 +9,21 @@ describe 'FizzBuzz' do
     it 'NOT divisible by three' do
       expect(is_divisible_by_three?(1)).to be false
     end
+
+    it 'divisible by five' do
+      expect(is_divisible_by_five?(5)).to be true
+    end
+
+    it 'NOT divisible by five' do
+      expect(is_divisible_by_five?(1)).to be false
+    end
+
+    it 'divisible by three and five' do
+      expect(is_divisible_by_three_and_five?(15)).to be true
+    end
+
+    it 'NOT divisible by three and five' do
+      expect(is_divisible_by_three_and_five?(1)).to be false
+    end
   end
 end
