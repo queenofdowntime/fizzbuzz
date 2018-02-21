e8b02e84fafadfe0e8113d280066d91abfbb9e99
fouralarmfire <claudiaberesford@googlemail.com>
2018-02-21 20:49:36 +0000

    part 7

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,10 @@
 def is_divisible_by?(number, divisor)
   number % divisor == 0
 end
+
+def fizzbuzz_says(number)
+  return 'fizzbuzz' if is_divisible_by?(number, 15)
+  return 'fizz' if is_divisible_by?(number, 3)
+  return 'buzz' if is_divisible_by?(number, 5)
+  number
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -26,4 +26,22 @@ describe 'FizzBuzz' do
       expect(is_divisible_by?(1, 15)).to be false
     end
   end
+
+  context 'when playing the game, fizzbuzz says...' do
+    it '"fizz" when a number is divisible by 3' do
+      expect(fizzbuzz_says(3)).to eq 'fizz'
+    end
+
+    it '"buzz" when a number is divisible by 5' do
+      expect(fizzbuzz_says(5)).to eq 'buzz'
+    end
+
+    it '"fizzbuzz" when a number is divisible by 3 and 5' do
+      expect(fizzbuzz_says(15)).to eq 'fizzbuzz'
+    end
+
+    it 'the number when it is not divisible by 3 or 5' do
+      expect(fizzbuzz_says(1)).to eq 1
+    end
+  end
 end
