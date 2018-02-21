b021c82b318a52d3286040a4ba1f2a253c934a77
fouralarmfire <claudiaberesford@googlemail.com>
2018-02-21 20:41:26 +0000

    part 6

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,3 @@
-def is_divisible_by_three?(number)
-  number % 3 == 0
-end
-
-def is_divisible_by_five?(number)
-  number % 5 == 0
-end
-
-def is_divisible_by_three_and_five?(number)
-  number % 15 == 0
+def is_divisible_by?(number, divisor)
+  number % divisor == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,27 +3,27 @@ require_relative '../lib/fizzbuzz.rb'
 describe 'FizzBuzz' do
   context 'knows when a number is' do
     it 'divisible by three' do
-      expect(is_divisible_by_three?(3)).to be true
+      expect(is_divisible_by?(3, 3)).to be true
     end
 
     it 'NOT divisible by three' do
-      expect(is_divisible_by_three?(1)).to be false
+      expect(is_divisible_by?(1, 3)).to be false
     end
 
     it 'divisible by five' do
-      expect(is_divisible_by_five?(5)).to be true
+      expect(is_divisible_by?(5, 5)).to be true
     end
 
     it 'NOT divisible by five' do
-      expect(is_divisible_by_five?(1)).to be false
+      expect(is_divisible_by?(1, 5)).to be false
     end
 
     it 'divisible by three and five' do
-      expect(is_divisible_by_three_and_five?(15)).to be true
+      expect(is_divisible_by?(15, 15)).to be true
     end
 
     it 'NOT divisible by three and five' do
-      expect(is_divisible_by_three_and_five?(1)).to be false
+      expect(is_divisible_by?(1, 15)).to be false
     end
   end
 end
